5502a78e496c8968e7e1539214287e4ed3e31eb2
DARK-02 <76576010+DARK-02@users.noreply.github.com>
2022-02-13 11:36:52 +0700

    Update run.php

--- run.php
+++ run.php
@@ -426,8 +426,7 @@ sleep(3);
 }
 else{
 echo "\033[0m\033[0;32m«\033[0;31m#\033[0;32m» \033[0mScript Update! \033[0;32m«\033[0;31m#\033[0;32m»";
-echo $d1a;
-//system("git pull");
-//system("php run.php");
+system("git pull");
+system("php run.php");
 }
 ?>
