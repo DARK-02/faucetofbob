f4288db7e48dce2371e1bf18abea33c3039081fa
DARK-02 <76576010+DARK-02@users.noreply.github.com>
2022-02-13 11:33:12 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,18 @@
+<?php
+
+//masukkan cookie lu
+$cookie = "_ga=GA1.2.60152842.1642315507; PHPSESSID=8uuosmtr2r7nao3vfg78tnv7hk; _gid=GA1.2.1888936768.1644199478; SesHashKey=btur1gv1xjtps69u; SesToken=ses_id=46664&ses_key=btur1gv1xjtps69u; AccExist=46664; _gat_gtag_UA_145431796_1=1";
+//masukkan user agent lu
+$useragent = "Mozilla/5.0 (Linux; Android 9; SM-T825Y) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.104 Safari/537.36";
+
+/*
+pilih waktu sesuka hati, recomend 60
+2 = 120
+5 = 300
+10 = 600
+contoh: $waktu_pencari = "60";
+*/
+$waktu_pencari = "60";
+
+//link ga usah di ganti :v
+$link = "https://faucetofbob.xyz/scratch.html";
